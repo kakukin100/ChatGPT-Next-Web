10dfd989e81c62eaa32a19260baf1235c6bcb6cc
kakukin100 <133530747+kakukin100@users.noreply.github.com>
2023-12-20 20:37:06 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -153,6 +153,10 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
+  {
+    name: "gemini-pro",
+    available: true,
+  },
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
