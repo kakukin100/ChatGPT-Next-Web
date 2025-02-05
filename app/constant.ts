a3886909fa52d8bf40092cc612140fa5b366dfad
kakukin100 <133530747+kakukin100@users.noreply.github.com>
2025-02-05 11:32:01 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -111,6 +111,8 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-0125-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
+  "gpt-4o": "2023-10",
+  "gpt-4o-mini": "2023-10",
   // After improvements, 
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
   "gemini-pro": "2023-12",
